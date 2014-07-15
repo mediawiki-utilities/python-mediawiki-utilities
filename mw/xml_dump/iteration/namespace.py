5ebd7a94203a66083a30e56cf0942a91e4103bfa
YuviPanda <yuvipanda@gmail.com>
2014-07-16 05:16:27 +0530

    pep8: Fix xml dumps subsystem
    Only fixes formatting issues. There are still:
    
    - Unused imports
    - Unused variables
    - Undefined variables being used
    - Line length standards to adopt

--- mw/xml_dump/iteration/namespace.py
+++ mw/xml_dump/iteration/namespace.py
@@ -1,12 +1,11 @@
 from ... import types
 
+
 class Namespace(types.Namespace):
-		
-	@classmethod
-	def from_element(cls, element):
-		return cls(
-			element.attr('key'),
-			element.text,
-			case=element.attr('case')
-		)
-	
+    @classmethod
+    def from_element(cls, element):
+        return cls(
+            element.attr('key'),
+            element.text,
+            case=element.attr('case')
+        )
