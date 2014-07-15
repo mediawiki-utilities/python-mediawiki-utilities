fe4f6d4fba19e06555b48536a8f9be6a607fe43a
YuviPanda <yuvipanda@gmail.com>
2014-07-16 05:16:27 +0530

    pep8: Fix xml dumps subsystem
    Only fixes formatting issues. There are still:
    
    - Unused imports
    - Unused variables
    - Undefined variables being used
    - Line length standards to adopt

--- mw/xml_dump/errors.py
+++ mw/xml_dump/errors.py
@@ -1,11 +1,12 @@
 class FileTypeError(Exception):
-	"""
-	Thrown when an XML dump file is not of an expected type. 
-	"""
-	pass
+    """
+    Thrown when an XML dump file is not of an expected type.
+    """
+    pass
+
 
 class MalformedXML(Exception):
-	"""
-	Thrown when an XML dump file is not formatted as expected. 
-	"""
-	pass
+    """
+    Thrown when an XML dump file is not formatted as expected.
+    """
+    pass
