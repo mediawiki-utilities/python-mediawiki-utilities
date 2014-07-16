1f57e8a098cfebd585b08fe3e19a954ae5ca7427
YuviPanda <yuvipanda@gmail.com>
2014-07-16 06:01:37 +0530

    pep8: Types subsystem

--- mw/types/namespace.py
+++ mw/types/namespace.py
@@ -2,37 +2,39 @@ from ..util import none_or
 
 from . import serializable
 
+
 class Namespace(serializable.Type):
-	"""
-	Namespace meta data.
-	"""
-	
-	__slots__ = ('id', 'name', 'aliases', 'case', 'canonical')
-	
-	def __init__(self, id, name, canonical=None, aliases=None, case=None):
-		self.id = int(id)
-		"""
-		Namespace ID : `int`
-		"""
-		
-		self.name = str(name)
-		"""
-		Namespace name : `str`
-		"""
-		
-		self.aliases = serializable.Set.deserialize(aliases or [], str)
-		"""
-		Alias names : set( `str` )
-		"""
-		
-		self.case = none_or(case, str)
-		"""
-		Case sensitivity : `str` | `None`
-		"""
-		
-		self.canonical = none_or(canonical, str)
-		"""
-		Canonical name : `str` | `None`
-		"""
-	
-	def __hash__(self): return self.id
+    """
+    Namespace meta data.
+    """
+
+    __slots__ = ('id', 'name', 'aliases', 'case', 'canonical')
+
+    def __init__(self, id, name, canonical=None, aliases=None, case=None):
+        self.id = int(id)
+        """
+        Namespace ID : `int`
+        """
+
+        self.name = str(name)
+        """
+        Namespace name : `str`
+        """
+
+        self.aliases = serializable.Set.deserialize(aliases or [], str)
+        """
+        Alias names : set( `str` )
+        """
+
+        self.case = none_or(case, str)
+        """
+        Case sensitivity : `str` | `None`
+        """
+
+        self.canonical = none_or(canonical, str)
+        """
+        Canonical name : `str` | `None`
+        """
+
+    def __hash__(self):
+        return self.id
