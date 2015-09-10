275cc4e7ff120c379a80e3c2a370578e01b0d9cf
halfak <aaron.halfaker@gmail.com>
2015-09-10 13:55:40 -0500

    Sets a default timeout for requests at 20 seconds unless otherwise specified.

--- mw/util/api.py
+++ mw/util/api.py
@@ -1,16 +1,19 @@
-import time
 import logging
+import time
 
 import requests
 
-logger = logging.getLogger("mw.util.api.session")
-
 from .functions import none_or
 
+logger = logging.getLogger("mw.util.api.session")
+
 FAILURE_THRESHOLD = 5
+TIMEOUT = 20
+
 
 class Session:
-    def __init__(self, uri, headers=None, failure_threshold=None, wait_step=2):
+    def __init__(self, uri, headers=None, timeout=None,
+                 failure_threshold=None, wait_step=2):
         if uri is None:
             raise TypeError("uri must not be None")
 
@@ -19,6 +22,7 @@ class Session:
         self.session = requests.Session()
 
         self.failure_threshold = int(failure_threshold or FAILURE_THRESHOLD)
+        self.timeout = float(TIMEOUT)
         self.wait_step = float(wait_step)
 
         self.failed = 0
@@ -34,10 +38,11 @@ class Session:
 
     def request(self, type, params):
         try:
-            result = self.session.request(type, self.uri, params=params)
+            result = self.session.request(type, self.uri, params=params,
+                                          timeout=self.timeout)
             self.failed = 0
             return result
-        except (requests.HTTPError, requests.ConnectionError, requests.Timeout):
+        except (requests.HTTPError, requests.ConnectionError):
             self.failed += 1
 
             if self.failed > self.failure_threshold:
